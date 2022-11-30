2c2a6bb397c8e8f9b80913273437012a4ecf428d
pclarytx22 <patrickm@theclarys.org>
2022-11-29 23:17:28 -0600

    Add Flexbot Styling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("My app.js file is attached")
+
+$('#clearText').on('click',function(){
+    //your javacript
+    console.log("Clear local storage")
+});
+
+// var exampleEl = document.getElementById('clearText')
+// var tooltip = new bootstrap.Tooltip(exampleEl, trigger:"hover")

900f80c317182cbf041dd33e7c154d9036135a3c
Patrick Clary <patrickm@theclarys.org>
2022-11-30 15:56:07 -0600

    Add API call to global endpoint for MC & TTL #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// console.log("My app.js file is attached")
-const apiRoot ="https://pro-api.coingecko.com/api/v3/"
+let globalData, coinList, $newCrypto
+
+const apiRoot ="https://api.coingecko.com/api/v3/"
+// API Methods
+const global = 'global' //get cc global data
+const getList = 'coins/list' //list of all supported cc, cache for later queries
+
+const $ttlCurrencies = $('#ttlCurrencies');
+const $ttlMarketCap = $('#ttlMarketCap');
 
 $('#clearText').on('click',function() {
     //your javacript
@@ -7,7 +14,27 @@ $('#clearText').on('click',function() {
 });
 
 $('#submitBtn').on('click', function(evt) {
-    const $newCrypto = $('#cryptoInput').prop('value');
+    $newCrypto = $('#cryptoInput').prop('value');
     console.log($newCrypto);
     $('#cryptoInput').prop('value','');
 });
+
+// code snipet to format market cap
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+$.ajax({
+    url:apiRoot + global
+}).then(
+    (data) => {
+        globalData = data;
+        console.log(globalData);
+        let tempTCC = numberWithCommas(globalData.data.active_cryptocurrencies)
+        let tempTMC = numberWithCommas(globalData.data.total_market_cap.usd.toFixed(0))
+        $ttlCurrencies.text(`${tempTCC}`);
+        $ttlMarketCap.text(`$${tempTMC}`);
+    }
+);
+
+
